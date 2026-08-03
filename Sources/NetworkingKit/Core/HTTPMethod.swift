3171330ab2b9dca1f2bdf525225dbb9e7eb169cc
Keshav Raj <keshv.raj@yahoo.com>
2026-08-03 14:17:22 +0530

    feat(Core): Add http method enum

---
+++ Sources/NetworkingKit/Core/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  NetworkingKit
+//
+//  Created by Keshav Raj on 03/08/26.
+//
+
+/// Represents the supported HTTP method
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
--- Sources/NetworkingKit/NetworkingKit.swift
+++ Sources/NetworkingKit/NetworkingKit.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct NetworkingKit {
+    
+    public init () {}
+    
+    public var version: String {
+        return "0.0.1"
+    }
+}
