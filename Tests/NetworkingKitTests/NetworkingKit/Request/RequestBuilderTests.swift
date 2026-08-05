deda466f1656b981b961f0e94e599e60bf840a03
Keshav Raj <keshv.raj@yahoo.com>
2026-08-05 16:17:13 +0530

    test(request): add URL construction tests

--- Tests/NetworkingKitTests/NetworkingKit/Request/RequestBuilderTests.swift
+++ Tests/NetworkingKitTests/NetworkingKit/Request/RequestBuilderTests.swift
@@ -6,21 +6,38 @@
 //
 
 import XCTest
+import Foundation
 @testable import NetworkingKit
 final class RequestBuilderTests: XCTestCase {
-    
-    private var endpoint: TestEndpoint!
+    private var path: String!
     private var baseURL: URL!
     
-    override func setUp()  {
-        let path = "/problems/permutation-string/history"
+    override func setUp() {
+        super.setUp()
+        path = "/problems/permutation-string/history"
         baseURL = URL(string: "https://leetcode.com")!
-        endpoint = TestEndpoint(path: path, method: .get)
     }
     
     func test_build_returnsURLRequestForValidEndpoint() throws {
+        let endpoint = TestEndpoint(path: path, method: .get)
         let request = try RequestBuilder.build(endpoint, baseURL: baseURL)
         XCTAssertEqual(request.url, URL(string: "https://leetcode.com/problems/permutation-string/history"))
+    }
+    
+    func test_build_removesTrailingSlashFromBaseURL() throws {
+        let baseURL = URL(string: "https://leetcode.com/")!
+        let endpoint = TestEndpoint(path: path, method: .get)
+        let request = try RequestBuilder.build(endpoint, baseURL: baseURL)
+        XCTAssertEqual(request.url, URL(string: "https://leetcode.com/problems/permutation-string/history"))
+    }
+    
+    func test_build_appendsQueryItemsToURL() throws {
+        let endpoint = TestEndpoint(path: path,
+                                    method: .get,
+                                    queryItems: [URLQueryItem(name: "page", value: "1"),
+                                                 URLQueryItem(name: "limit", value: "20")])
+        let request = try RequestBuilder.build(endpoint, baseURL: baseURL)
+        XCTAssertEqual(request.url, URL(string: "https://leetcode.com/problems/permutation-string/history?page=1&limit=20"))
     }
 
 }
--- Tests/NetworkingKitTests/NetworkingKit/TestHelpers/TestEndpoint.swift
+++ Tests/NetworkingKitTests/NetworkingKit/TestHelpers/TestEndpoint.swift
@@ -11,4 +11,11 @@ import Foundation
 struct TestEndpoint: Endpoint {
     let path: String
     let method: HTTPMethod
+    let queryItems: [URLQueryItem]
+    
+    init(path: String, method: HTTPMethod, queryItems: [URLQueryItem] = []) {
+        self.path = path
+        self.method = method
+        self.queryItems = queryItems
+    }
 }
